41f4040c69a030d576635b3306c80c8e53e76360
xamely <m.xeniya.a@ya.ru>
2018-12-23 02:48:23 +0300

    Second page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   root 'log_page#index'
+
+  get 'registration' => 'log_page#registration'
 end
